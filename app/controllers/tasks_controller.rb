fa4149d695950cffd4fcab1293a4cd3d41720c2c
Hannah Brett <hannahebrett89@icloud.com>
2023-11-24 14:40:21 +0000

    Complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
+    #@task = Task.find(params[:id])
   end
 
   def new
@@ -28,6 +31,7 @@ class TasksController < ApplicationController
   end
 
   def edit
+    #@task = Task.find(params[:id])
   end
 
   def destroy
